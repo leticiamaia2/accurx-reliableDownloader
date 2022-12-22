339e6270f42d1bd11b03642cdaaf7fbca50039f3
leticiamaia2 <121205492+leticiamaia2@users.noreply.github.com>
2022-12-22 09:52:52 -0300

    Initial commit

---
+++ dot-net/ReliableDownloader/FileDownloader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ReliableDownloader;
+
+public class FileDownloader : IFileDownloader
+{
+    public Task<bool> DownloadFile(string contentFileUrl, string localFilePath, Action<FileProgress> onProgressChanged)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CancelDownloads()
+    {
+        throw new NotImplementedException();
+    }
+}
